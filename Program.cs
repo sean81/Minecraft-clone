9125be9fb7ad1f27effbed5d744c361d061ea3a0
Asorus <Asorus@DESKTOP-ODDETGD>
2024-02-14 21:43:19 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Minecraft_clone
+{
+
+    class Program
+    {
+        // Entry point of the program
+        static void Main(string[] args)
+        {
+            // Creates game object and disposes of it after leaving the scope
+            using(Game game = new Game(500, 500))
+            {
+                // Running the game
+                game.Run();
+            }
+        }
+    }
+}
